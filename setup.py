18180177ee2763d951a23fc05052f7995d63a05b
Manish Raj <60720070+Cre5cend0@users.noreply.github.com>
2022-12-26 02:00:50 +0530

    Updating folder structure

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(file_name):
 
 setup(
     name='mac_voice_assistant',
-    version='0.0.3',
+    version='0.1.0',
     packages=find_packages(),
     url='https://pypi.org/project/mac-voice-assistant/',
     license='MIT',
